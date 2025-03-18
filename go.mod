d0947958f9003b9bbb06e26b38aec0d0b4696479
Tae Park <tae.park@windriver.com>
2025-03-18 15:57:24 -0400

    Create config package and include host DNS configs
    Creating a new config package for handling openbao monitor service
    configurations. Adding a functionality for handling host DNS configs.
    
    Signed-off-by: Tae Park <tae.park@windriver.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/michel-thebeau-WR/openbao-manager-go/baomon
 
 go 1.18
+
+replace github.com/michel-thebeau-WR/openbao-manager-go/baomon/config => ./config
+
+require github.com/michel-thebeau-WR/openbao-manager-go/baomon/config v0.0.0-00010101000000-000000000000
+
+require (
+	github.com/go-yaml/yaml v2.1.0+incompatible // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	baoConfig "github.com/michel-thebeau-WR/openbao-manager-go/baomon/config"
+)
 
 func main() {
-	fmt.Print("Baomon says \"hello world!\"\n")
+
+	if len(os.Args) < 2 {
+		fmt.Print("No other functionality is supported at the moment.\n")
+		return
+	}
+
+	args := os.Args[1:]
+	if args[0] == "--read" || args[0] == "-r" {
+		if len(args) < 2 {
+			fmt.Fprint(os.Stderr, "Invalid number of arguments for --read/-r\n")
+			return
+		}
+
+		var S baoConfig.MonitorConfig
+		openedFile, err := os.Open(args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to open file %v. Error message: %v\n", args[1], err)
+			return
+		}
+		defer openedFile.Close()
+
+		err = S.ReadYAMLMonitorConfig(openedFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error with parsing read yaml data: %v\n", err)
+			return
+		}
+		fmt.Printf("Result: \n%#v\n", S)
+	} else if args[0] == "--write" || args[0] == "-w" {
+		if len(args) < 3 {
+			fmt.Fprint(os.Stderr, "Invalid number of arguments for --read/-r\n")
+			return
+		}
+
+		var S baoConfig.MonitorConfig
+		openedFile, err := os.Open(args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to open file %v. Error message: %v\n", args[1], err)
+			return
+		}
+		defer openedFile.Close()
+
+		err = S.ReadYAMLMonitorConfig(openedFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error with parsing read yaml data: %v\n", err)
+			return
+		}
+		fmt.Printf("Read result: \n%#v\n", S)
+
+		writeFile, err := os.Create(args[2])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to open %v for writing\n", args[2])
+			return
+		}
+		defer writeFile.Close()
+		err = S.WriteYAMLMonitorConfig(writeFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error with writing parsed yaml data to file: %v\n", err)
+			return
+		}
+		fmt.Print("Write Complete\n")
+	} else {
+		fmt.Print("No other functionality is supported at the moment.\n")
+		return
+	}
 }
